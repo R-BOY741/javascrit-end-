9559d328a17b601bbe13bfe36cdf6afa726e0307
lifechoices <leratolifechoices741@gmai.com>
2021-08-12 15:51:15 +0200

    updated

---
+++ script.js
@@ -0,0 +1,6 @@
+let cart_botton = document.querySelector(".cart2");
+let blob = document.querySelector("#cart2");
+
+cart_botton.addEventListener("click", () => {
+  blob.classList.toggle("active");
+});
