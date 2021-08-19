fab0bb58287242b27b1afcc4131cfdda4019371b
lifechoices <leratolifechoices741@gmai.com>
2021-08-19 13:02:31 +0200

    updated

--- login.js
+++ login.js
@@ -1,33 +1,27 @@
+let signInButton = document.querySelector("form");
 
 function login(username, password) {
-    console.log(username);
-    console.log(password);
-    fetch("https://still-ocean-71718.herokuapp.com/auth", {
-      method: "POST",
-      body: JSON.stringify({
-        username: ${username},
-        password: ${password},
-      }),
-      headers: {
-        "Content-Type": "application/json",
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        if (data["access_token"]) {
-          console.log(data);
-          myStorage = window.localStorage;
-          myStorage.setItem("jwt-token", data["access_token"]);
-          myStorage.setItem("username", username);
-          myStorage.setItem("password", password);
-          window.location.href = "./scripts/account.html";
-        }
-      });
-  }
-  
-  document.querySelector(".login-form").addEventListener("submit", (e) => {
-    let username = document.querySelector(".username").value;
-    let password = document.querySelector(".password").value;
-    e.preventDefault();
-    login(username, password);
-  });
+  console.log(username);
+  console.log(password);
+  fetch("https://enigmatic-mesa-96671.herokuapp.com/auth", {
+    method: "POST",
+    body: JSON.stringify({
+      username: `${username}`,
+      password: `${password}`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data["access_token"]) {
+        console.log(data);
+        myStorage = window.localStorage;
+        myStorage.setItem("jwt-token", data["access_token"]);
+        myStorage.setItem("username", username);
+        myStorage.setItem("password", password);
+        window.location.href = "./main.html";
+      }
+    });
+}
