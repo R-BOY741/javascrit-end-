c549b093664ccc84a515c9bd5640c078a97dd4ee
lifechoices <leratolifechoices741@gmai.com>
2021-09-15 13:22:35 +0200

    first commit

--- login.js
+++ login.js
@@ -1,13 +1,14 @@
-let signInButton = document.querySelector("form");
+let signInButton = document.querySelector("#login-button");
 
-function login(username, password) {
-  console.log(username);
-  console.log(password);
-  fetch("https://enigmatic-mesa-96671.herokuapp.com/auth", {
-    method: "POST",
+function login() {
+  let username = document.querySelector("#username").value;
+  let password = document.querySelector("#password").value;
+
+  fetch(" https://enigmatic-mesa-96671.herokuapp.com//user-registration/", {
+    method: "PATCH",
     body: JSON.stringify({
-      username: `${username}`,
-      password: `${password}`,
+      username,
+      password,
     }),
     headers: {
       "Content-Type": "application/json",
@@ -15,12 +16,9 @@ function login(username, password) {
   })
     .then((response) => response.json())
     .then((data) => {
-      if (data["access_token"]) {
-        console.log(data);
-        myStorage = window.localStorage;
-        myStorage.setItem("jwt-token", data["access_token"]);
-        myStorage.setItem("username", username);
-        myStorage.setItem("password", password);
+      console.log(data);
+      if (data.data) {
+        localStorage.setItem("user", JSON.stringify(data.data));
         window.location.href = "./main.html";
       }
     });
